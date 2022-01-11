05742a4df8c7e7b6280b40015cf850cba7a32040
andresbertone <andresbertone97@gmail.com>
2022-01-11 19:13:25 -0300

    Fix: Se agregan alias a las asociaciones

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -13,39 +13,39 @@ const Status = require('./status');
 // Associations
 const initAssociations = () => {
         // tipo_reclamo & subcategoria_reclamo
-    ClaimType.hasMany(ClaimSubcategory, { foreignKey: 'idTipoReclamo' });
-    ClaimSubcategory.belongsTo(ClaimType, { foreignKey: 'idTipoReclamo' });
+    ClaimType.hasMany(ClaimSubcategory, { foreignKey: 'claimTypeId', as: 'claimSubcategory' });
+    ClaimSubcategory.belongsTo(ClaimType, { foreignKey: 'claimTypeId', as: 'claimType' });
 
         // vecino & contacto
-    Neighbor.hasMany(Contact, { foreignKey: 'idVecino' });
-    Contact.belongsTo(Neighbor, { foreignKey: 'idVecino' });
+    Neighbor.hasMany(Contact, { foreignKey: 'neighborId', as: 'contacts' });
+    Contact.belongsTo(Neighbor, { foreignKey: 'neighborId', as: 'neighbor' });
 
         // agente_municipal & reclamo
-    MunicipalAgent.hasMany(Claim, { foreignKey: 'idAgenteMunicipal', onUpdate: 'CASCADE', onDelete: 'RESTRICT' });
-    Claim.belongsTo(MunicipalAgent, { foreignKey: 'idAgenteMunicipal' });
+    MunicipalAgent.hasMany(Claim, { foreignKey: 'municipalAgentId', as: 'claim', onUpdate: 'CASCADE', onDelete: 'RESTRICT' });
+    Claim.belongsTo(MunicipalAgent, { foreignKey: 'municipalAgentId', as: 'municipalAgent' });
 
         // subcategoria_reclamo & reclamo
-    ClaimSubcategory.hasMany(Claim, { foreignKey: 'idSubcategoriaReclamo', onUpdate: 'CASCADE', onDelete: 'RESTRICT' });
-    Claim.belongsTo(ClaimSubcategory, { foreignKey: 'idSubcategoriaReclamo' });
+    ClaimSubcategory.hasMany(Claim, { foreignKey: 'claimSubcategoryId', as: 'claim',onUpdate: 'CASCADE', onDelete: 'RESTRICT' });
+    Claim.belongsTo(ClaimSubcategory, { foreignKey: 'claimSubcategoryId', as: 'claimSubcategory' });
 
         // tipo_hecho_de_inseguridad & reclamo
-    InsecurityFactType.hasMany(Claim, { foreignKey: 'idTipoHechoDeInseguridad', onUpdate: 'CASCADE', onDelete: 'RESTRICT' });
-    Claim.belongsTo(InsecurityFactType, { foreignKey: 'idTipoHechoDeInseguridad' });
+    InsecurityFactType.hasMany(Claim, { foreignKey: 'insecurityFactTypeId', as: 'claim', onUpdate: 'CASCADE', onDelete: 'RESTRICT' });
+    Claim.belongsTo(InsecurityFactType, { foreignKey: 'insecurityFactTypeId', as: 'insecurityFactType' });
 
         // vecino & reclamo
-    Neighbor.hasMany(Claim, { foreignKey: 'idVecino', onUpdate: 'CASCADE', onDelete: 'RESTRICT' });
-    Claim.belongsTo(Neighbor, { foreignKey: 'idVecino' });
+    Neighbor.hasMany(Claim, { foreignKey: 'neighborId', as: 'claim', onUpdate: 'CASCADE', onDelete: 'RESTRICT' });
+    Claim.belongsTo(Neighbor, { foreignKey: 'neighborId', as: 'neighbor' });
 
         // reclamo & estado a través de la tabla estado_reclamo
-    Claim.belongsToMany(Status, { through: StatusClaim, foreignKey: 'idReclamo', otherKey: 'idEstado' });
-    Status.belongsToMany(Claim, { through: StatusClaim, foreignKey: 'idEstado', otherKey: 'idReclamo' });
+    Claim.belongsToMany(Status, { through: StatusClaim, foreignKey: 'claimId', otherKey: 'statusId', as: 'status_claim' }); // TODO: Revisar que el 'as' esté correcto
+    Status.belongsToMany(Claim, { through: StatusClaim, foreignKey: 'statusId', otherKey: 'claimId', as: 'status_claim' }); // TODO: Revisar que el 'as' esté correcto
 
         // reclamo & vecino a través de la tabla favoritos
-    Claim.belongsToMany(Neighbor, { through: Favorites, foreignKey: 'idReclamo', otherKey: 'idVecino' });
-    Neighbor.belongsToMany(Claim, { through: Favorites, foreignKey: 'idVecino', otherKey: 'idReclamo' });
+    Claim.belongsToMany(Neighbor, { through: Favorites, foreignKey: 'claimId', otherKey: 'neighborId', as: 'favorites' }); // TODO: Revisar que el 'as' esté correcto
+    Neighbor.belongsToMany(Claim, { through: Favorites, foreignKey: 'neighborId', otherKey: 'claimId', as: 'favorites' }); // TODO: Revisar que el 'as' esté correcto
 };
 
-initAssociations(); // TODO: Revisar que este método funcione bien cuando hagamos un insert o algún otro tipo de operación sobre la base de datos.
+initAssociations();
 
 
 module.exports = {
