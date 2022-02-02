f9e63b35ab8f46199599b988d560d2f86f470806
andresbertone <andresbertone97@gmail.com>
2022-02-02 14:14:49 -0300

    Add: Comentarios

--- backend/src/routes/claimRouter.js
+++ backend/src/routes/claimRouter.js
@@ -8,8 +8,13 @@ const claimController = require('../controllers/claimController');
 router.get('/favorites', verifyToken, claimController.getFavoriteClaims);
 router.get('/pending', verifyToken, claimController.getPendingClaims);
 router.get('/:claimId', verifyToken, claimController.getClaimById);
+
+// * Se usa tanto para crear un nuevo reclamo como un nuevo hecho de inseguridad
 router.post('/', verifyToken, uploadImageErrorHandler, claimController.createClaim);
+
+// * Se usa tanto para actualizar un reclamo como un hecho de inseguridad
 router.put('/:claimId', verifyToken, uploadImageErrorHandler, claimController.editClaim);
+
 router.put('/updateStatus/:claimId', verifyToken, claimController.changeStatusToClaim);
 router.delete('/:claimId', verifyToken, claimController.deleteClaim);
 
