616060a4174e2a9bae6deb29fd28157514110c3f
andresbertone <andresbertone97@gmail.com>
2023-07-13 20:36:06 -0300

    ajuste nombre de imagen

--- backend/src/utils/imageUtil.js
+++ backend/src/utils/imageUtil.js
@@ -1,16 +1,21 @@
 const cloudinary = require('cloudinary').v2;
 
+function getImageName() {
+    return Date.now() + '.jpg';
+}
 
 const saveImage = async (image) => {
     const options = {
         use_filename: true,
-        unique_filename: true,
+        // unique_filename: true,
         overwrite: true
     };
+
+    const imageName = getImageName();
   
     try {
         // Upload the image
-        const result = await cloudinary.uploader.upload(image, options);
+        const result = await cloudinary.uploader.upload(imageName, options);
         return result.secure_url;
     } catch (error) {
         console.log('ERROR LA SUBIR IMAGEN:', error);
